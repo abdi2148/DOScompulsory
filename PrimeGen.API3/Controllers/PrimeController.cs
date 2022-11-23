7318b157eacae563cf46f86338d0ebf4966f997d
abdi2148 <abdiqafar.abas@gmail.com>
2022-11-23 19:01:58 +0100

    Updated and functional

---
+++ PrimeGen.API3/Controllers/PrimeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeGen.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace PrimeGen.API3.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PrimeController : ControllerBase
+    {
+        private readonly IPrimeService _service;
+
+        public PrimeController(IPrimeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("isprime")]
+        public ActionResult<bool> IsPrime([FromQuery] string input)
+        {
+            try
+            {
+                Thread.Sleep(3000);
+                return Ok(_service.IsPrime(input));
+            }
+            catch (FormatException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("countprimes")]
+        public ActionResult<List<string>> CountPrimes([FromQuery] string start, string end)
+        {
+            try
+            {
+                return Ok(_service.CountPrimes(start, end));
+            }
+            catch (FormatException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
